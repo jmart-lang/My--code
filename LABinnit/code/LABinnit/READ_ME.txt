6fce6c9227137018407b4c3d8d6d2583a9e79bd1
jmart-lang <122550311+jmart-lang@users.noreply.github.com>
2023-03-22 22:53:07 +0100

    Add files via upload

---
+++ LABinnit/code/LABinnit/READ_ME.txt
@@ -0,0 +1,87 @@
+--------------------------------------------------------------------------------------------------------------------------------
+
+Vidim da si nalozil mojo igro, tako da imam zate samo se nekaj navodil:
+
+- ta igrica potrebuje knjiznico pygame, kar pomeni da jo moras naloziti
+- nalozis jo tako, da v terminal vtipkas naslednji ukaz: pip install pygame
+- terminal odpres tako, da pritisnes tipki Win + R in v okence, ki se pojavi vtipkas cmd in pritisnes tipko Enter
+- prav tako moras naloziti Python IDLE, ki ga lahko nalozis na spletni strani: https://www.python.org/downloads/
+- ko to koncas, odpri mapo LABinnit in dvakrat klikni na datoteka main.py
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Osnovni napotki za igro:
+- premikas se lahko z uporabo awsd tipk
+	- a = levo
+	- w = gor
+	- s = dol
+	- d = desno
+- cilj igre je pobegniti iz labirinta
+- iz labirinta pobegnes tako, da gres skozi moder portal
+- ce izberes srednjo ali pa legendary tezavnost, bi lahko generiranje labirinta vzelo malo vec casa
+- zaradi zgornjih razlogov prosim za potrpljenje
+- ce po dolgotrajnem cakanju igra se kar ne generira labirinta samo spammaj gumb z izbrano tezavnostjo - to ponavadi pomaga
+- ce izberes lahko tezavnost, bos imel na voljo samo en portal, ce pa srednjo ali pa legendarno, pa bos imel na voljo 2 portala
+- en se bo nahajal nekje ob skrajno spodnji steni, en pa nekje ob skrajno desni steni, oba sta nakljucno postavljena
+- po koncani igri bos dobil moznost za ponovno igranje, ali pa da igro koncas. izberi, kar hoces
+- in se glavna stvar: uzivaj!
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Moji viri pri programiranju:
+- https://i.stack.imgur.com/PBde2.gif 
+	- navdih za moj avtorski lik igralca
+- https://www.youtube.com/watch?v=cwWi05Icpw0
+	- tutorial, s pomocjo katerega sem igrico naredil
+- https://github.com/clear-code-projects/SimpleZelda
+	- github stran, kjer je zbrene vsa koda, ki je bila napisana v tutorialu (ni moja koda)
+- https://medium.com/swlh/fun-with-python-1-maze-generator-931639b4fb7e
+	- razlaga programa za nakljucno generiranje labirinta
+- https://github.com/OrWestSide/python-scripts/blob/master/maze.py
+	- program za nakljucno generiran labirint ()
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Spletna stran s katero sem si pomagal pri dizajniranju graficne podobe moje igrice
+- https://www.pixilart.com/mart2
+	- to je moj profil, tam lahko vidite vse sprite, ki sem jih narisal in tudi timelapse njihovega nastanka
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Mentorica projektne naloge: prof. Helena Starc Grlj
+Avtor projektne naloge: Jurij Martinčič, 2D
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+                       .,,uod8B8bou,,.
+              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.
+         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||
+         !...:!TVBBBRPFT||||||||||!!^^""'   ||||
+         !.......:!?|||||!!^^""'            ||||
+         !.........||||                     ||||
+         !.........||||  ##                 ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         `.........||||                    ,||||
+          .;.......||||               _.-!!|||||
+   .,uodWBBBBb.....||||       _.-!!|||||||||!:'
+!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....
+!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   `.
+!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     `.
+!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"`;:::       `.
+!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.
+`..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
+  `..........:::::::::::::::::::::::;iof688888888888b.     `YBBBP^'
+    `........::::::::::::::::;iof688888888888888888888b.     `
+      `......:::::::::;iof688888888888888888888888888888b.
+        `....:::;iof688888888888888888888888888888888899fT!
+          `..::!8888888888888888888888888888888899fT|!^"'
+            `' !!988888888888888888888888899fT|!^"'
+                `!!8888888888888888899fT|!^"'
+                  `!988888888899fT|!^"'
+                    `!9899fT|!^"'
+                      `!^"'
+
+--------------------------------------------------------------------------------------------------------------------------------
---
+++ LABinnit/code/LABinnit/code/player.py
@@ -0,0 +1,141 @@
+import pygame
+from settings import *
+import os
+
+# to je zadnja stvar, ki jo dodajam, dodal bom kodo, ki bo omogocila, da ta koda tece na kateremkoli racunalniku
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(script_dir, '..', 'graphics', 'test',
+'Theseus_right.png')
+image_path2 = os.path.join(script_dir, '..', 'graphics', 'test',
+'Theseus_left.png')
+image_path3 = os.path.join(script_dir, '..', 'graphics', 'test',
+'Theseus_up.png')
+image_path4 = os.path.join(script_dir, '..', 'graphics', 'test',
+'Theseus_down.png')
+script_dir2 = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir2, '..', 'code', 'endcredits.py')
+
+# ustvarimo nov class
+# ta program sem skopiral iz tile.py, vse kar rabim narediti je samo spremeniti ime classa in pa popraviti nekaj manjsih zadev
+
+# spremenil sem ime classa v Player
+class Player(pygame.sprite.Sprite):
+    # pos = rabimo pozicijo vseh spritov
+    # groups = del katere skupine je sprite
+    # obstacle_sprites = vedeti je treba pozicije ovir, ce hocem, da collisons delujejo
+    def __init__(self,pos,groups,obstacle_sprites,portal_sprites): #  <--
+        # zastartamo clas v vrstici 5 
+        super().__init__(groups)
+        # zato ker imamo tukaj zgoraj znotraj oklepaja groups, lahko uporabimo spodnja izraza, saj sta nujna za vsak sprite
+        # tukaj hocem naloziti sliko, zato moram uporabiti pygame.image.load, ter dodati kje se nahaja in jo pretvoriti v obliko, 
+        ## ki jo zaslon lahko prikaze
+        # tukaj sem moral spremeniti pot do datoteke
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,0)
+
+        # dobili smo program, ki nam bo na zaslonu prikazoval igralca oz. njegov lik
+
+        # da mi 2 vektorja, ki sta navadno 0,0
+        # z pritiskom na tipkovnico se bo ta vrednost spreminjala
+        # rabimo vnos tipkovnice
+        self.direction = pygame.math.Vector2()
+        # damo mu se hitrost, s katero lahko pomnozimo premik in tako dobimo stalno giabnje
+        self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
+        self.portal_sprites = portal_sprites #  <--
+    
+    def input(self):
+        # vse tipke, ki so pritisnjene na tipkovnici
+        keys = pygame.key.get_pressed()
+        # ce pritisnem puscico ki kaze gor/dol, se bo lik premaknil gor/dol
+        # dodal sem moznost, da se spreminja slika igralcevega lika, glede na to, katero tipko pritisne
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+            self.image = pygame.image.load(image_path3).convert_alpha()
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+            self.image = pygame.image.load(image_path4).convert_alpha()
+        # ce ne dodamo tega else, bi se lik premikal naprej
+        else:
+            self.direction.y = 0
+
+        # ce pritisnem puscico ki kaze desno/levo, se bo lik premaknil desno/levo
+        # treba je spremeniti direction iz y v x in dodati vrednosti
+        # dodal sem moznost, da se spreminja slika igralcevega lika, glede na to, katero tipko pritisne
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.image = pygame.image.load(image_path).convert_alpha()
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.image = pygame.image.load(image_path2).convert_alpha()
+        # ce ne dodamo tega else, bi se lik premikal naprej
+        else:
+            self.direction.x = 0
+
+    # ustvarim nacin premikanja
+    def move(self,speed):
+        # to dodamo, da, ce se premikamo diagonalno, da se hitrosti ne sestevajo ampak vedno ostanejo enae
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        
+        # tukaj moramo dolociti horizontalno in vertikalno gibanje
+        self.hitbox.x += self.direction.x * speed
+        # ko premaknem lik igralca hocem preveriti horizontalne trke
+        self.collision('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        # ko premaknem igralca hocem preveriti vertikalne trke
+        self.collision('vertical')
+        #  prejsnja metoda --> self.rect.center += self.direction * speed
+        self.rect.center = self.hitbox.center
+    
+    # ustvarim novo funkcijo, ki bo obravnavala 'trke' med spriti
+    # damo smer, ki je lahko vertikalna ali horizontalna
+
+    def collision(self,direction):
+        # ce je smer horizontalna:
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                # preverimo, ce sprite igralca trci ob sprite ovire
+                if sprite.hitbox.colliderect(self.hitbox):
+                    # ne vem ali trci na desni ali na levi strani
+                    # predvidevam, da lahko trci z oviro samo v smeri, v katero se premika
+                    if self.direction.x > 0: #  <-- tukaj se premikam v desno (vektor gibanja je vecji od nic)
+                        self.hitbox.right = sprite.hitbox.left # vem da se premikam v desno kar pomeni da trcim z levo stranjo ovire 
+                        # hocem premakniti desno stran igralca zraven leve strani ovire
+                    if self.direction.x < 0: #  <-- tekaj se premikam v levo (vektor gibanja je manjsi od nic)
+                        self.hitbox.left = sprite.hitbox.right #  vem da se premikam v levo kar pomeni da trcim z desno stranjo ovire
+                        # igralca premaknem na desno stran ovire
+            # tuki naprej
+            for sprite in self.portal_sprites:
+                        if sprite.hitbox.colliderect(self.hitbox):
+                            pygame.display.quit() # zaprem pygame display window
+                            os.system(file_path) # zazenem datoteko endcredits.py
+                            quit() 
+
+        # ce je smer vertikalna
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                # preverimo, ce sprite igralca trci ob sprite ovire
+                if sprite.hitbox.colliderect(self.hitbox):
+                    # ne vem ali trci na zgornji ali na spodnji strani
+                    # predvidevam, da lahko trci z oviro samo v smeri, v katero se premika
+                    if self.direction.y > 0: #  <-- tekaj se premikam dol (vektor gibanja je manjsi od nic)
+                        self.hitbox.bottom = sprite.hitbox.top #  vem da se premikam dol kar pomeni da trcim z zgornjo stranjo ovire
+                        # igralca premaknem na zgornjo stran ovire
+                    if self.direction.y < 0: #  <-- tukaj se premikam gor (vektor gibanja je vecji od nic)
+                        self.hitbox.top = sprite.hitbox.bottom #  vem da se premikam gor kar pomeni da trcim s spodnjo stranjo ovire 
+                        # hocem premakniti zgornjo stran igralca zraven spodnje strani ovire
+            # tuki naprej
+            for sprite in self.portal_sprites:
+                        if sprite.hitbox.colliderect(self.hitbox):
+                            pygame.display.quit() # zaprem pygame display window
+                            os.system(file_path) # zazenem datoteko endcredits.py
+                            quit()
+                            
+
+    # igra samo sebe posodablja
+    def update(self):
+        self.input()
+        self.move(self.speed)
